d13d0efdca4a665d900d388254b1017f41420e8a
taseersuleman <75067547+taseersuleman@users.noreply.github.com>
2023-09-13 14:30:44 +0500

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -15,14 +15,16 @@ pre_prompt = "You are a helpful assistant. You do not respond as 'User' or prete
 
 prompt_input = st.text_area('Input your query')
 
+
+
 # Generate LLM response
 output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # LLM model
-                        input={"prompt": f"{pre_prompt} {prompt_input} Assistant: ", # Prompts
-                        "temperature":0.1, "top_p":0.9, "max_length":400, "repetition_penalty":1})  # Model parameters
+input={"prompt": f"{pre_prompt} {prompt_input} Assistant: ", # Prompts
+      "temperature":0.1, "top_p":0.9, "max_length":400, "repetition_penalty":1})  # Model parameters
 
 full_response = ""
 
-for item in output:
-  full_response += item
-
-st.write(full_response)
+if st.button("Submit"):
+    for item in output:
+        full_response += item
+    st.write(full_response)
